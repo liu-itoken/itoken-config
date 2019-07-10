d4f5242234b38c5a04d724aae4996b6f24405689
liuxiang <2395546591@qq.com>
2019-07-10 16:53:27 +0800

    增加eureka-dev配置文件

--- src/mian/java/com/liux/spring/cloud/itoken/config/ConfigApplication.java
+++ src/mian/java/com/liux/spring/cloud/itoken/config/ConfigApplication.java
@@ -1,4 +1,4 @@
-package com.liux.spring.cloud.itoken.config.Config;
+package com.liux.spring.cloud.itoken.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
